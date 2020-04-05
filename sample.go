73e11b4cad756966c1dea54d835acf210b94c2ee
Nivas Hegde <34051291+nivashegde@users.noreply.github.com>
2020-04-04 22:12:34 -0700

    Update sample.go

--- sample.go
+++ sample.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World!")
+    fmt.Fprintf(w, "Hello World! ")
     fmt.Println("Endpoint Hit: homePage")
 }
 
