37a344d0f708568dd1bc9d41792d0c1f23b67fde
Nivas Hegde <34051291+nivashegde@users.noreply.github.com>
2020-04-18 10:21:58 -0700

    Trigger build

--- sample.go
+++ sample.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "TEST@")
+    fmt.Fprintf(w, "Hello WOrld!!!")
     fmt.Println("Endpoint Hit: homePage")
 }
 
